ebd8e7179fc99f8ba6275ada7cb90de338b16b7e
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-02-20 11:44:56 +0200

    fix task 4

--- task4/handlers.py
+++ task4/handlers.py
@@ -3,8 +3,11 @@ from decorators import input_error
 @input_error
 def add_contact(args, contacts):
     name, phone = args
-    contacts[name] = phone
-    return "Contact added."
+    if name not in contacts:
+        contacts[name] = phone
+        return "Contact added."
+    else:
+        return "Contact exist"
 
 @input_error
 def change_contact(args, contacts):
